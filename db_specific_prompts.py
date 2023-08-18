f540c3d325a92ed5570584b9b61f401ad597442e
Greg Wilson <79663385+gregwdata@users.noreply.github.com>
2023-08-18 06:05:13 +0000

    adjust lfu prompt

--- db_specific_prompts.py
+++ db_specific_prompts.py
@@ -55,23 +55,15 @@ So to calculate the profit of an item, you need to subtract the supplycost field
 'lfu':
 """
 This is the Ladle furnace unit (LFU) dataset, with process data from metallurgical equipment for out-of-furnace steel processing in the converter shop. 
-The LFU is a large metal ladle with a volume of about 100 tons and is lined from the inside with refractory bricks. 
-Molten steel is poured into the LFU from a steel ladle and then heated by graphite electrodes inserted into the unit's lid.
-
-In addition to electric heating, desulfurization (removal of sulfur from the melt), adjustment of the chemical composition, and sampling are carried out at the CPC. 
-Alloying of metal is carried out both with lumpy ferroalloys through the system to supply bulk materials from bunkers and with wire materials through a tube apparatus. 
-Averaging the chemical composition and temperature is carried out through the bottom purge device in the steel ladle.
-
-After completion of processing at the LFU, the melt is poured back into the steel ladle and either sent to other metal finishing units or to a continuous casting plant, where it solidifies in the form of metallurgical slabs.
 
 Each time the melt enters the Ladle Furnace Unit, the initial temperature and chemical composition are measured. 
 Then, if necessary, the melt is heated for several minutes, after which alloying materials are added, purged with a gas, stirring the melt, 
-and measurements are again carried out following the approved Technological Instruction. This cycle is repeated several times until the 
-target chemistry and melting temperature are reached. In this case, it is not necessary that the melt would be heated in each cycle.
+and measurements are again carried out. This cycle is repeated several times until the target chemistry and melting temperature are reached. 
+In this case, it is not necessary that the melt would be heated in each cycle.
 
 The given tables provide information on the main technological operations performed at the Ladle Furnace Unit (such as heating, ferroalloy output, chemistry measurement, etc.) with a time reference.
 
-Each table in the database has a "key" column, which is the melt batch number. 
+Each table in the database has a "_key" column, which is the melt batch number. 
 Tables with columns with "time" in the title may have multiple rows related to the same melt key, and
 values from these tables may need to be aggregated at the melt level or have window or other functions applied,
 depending on the user's question.
@@ -84,6 +76,8 @@ The "temp" table and the "temp_FULL_with_test" table both contain temperature me
 If a user asks a question about temperature, you MUST use the "temp_FULL_with_test" table for temperature data. The "temp" table has missing values.
 
 All time values are stored as timestamps in the format YYYY-MM-DD HH:mm:ss. If you want to match strictly on a date, it is best to compare time to the date with >= AND < conditions.
+
+When you query, do not inlcude the "main." schema prefix on the table names, since we only have one schema.
 
 """,
 }
